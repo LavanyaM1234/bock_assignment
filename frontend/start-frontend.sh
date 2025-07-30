a0b1a7c5646736c2a0141123a4ed083cfbc54eb1
LavanyaM1234 <lavanyam7366@gmail.com>
2025-07-30 08:32:59 +0530

    Update start-frontend.sh

--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y python3
-cd /home/lavanyam_cs22/gpt2-chat-deployment/frontend
-nohup python3 -m http.server 80 &
+metadata_startup_script = <<-EOT
+    sudo apt update
+    sudo apt install -y python3 git
+    git clone https://github.com/${var.github_username}/gpt2-chat-deployment.git /opt/gpt2-chat-deployment
+    cd /opt/gpt2-chat-deployment/frontend
+    nohup python3 -m http.server 8080 &
+  EOT
