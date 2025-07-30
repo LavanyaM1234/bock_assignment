506668e883175c12ff2b51198b54d359b69ffe4d
LavanyaM1234 <lavanyam7366@gmail.com>
2025-07-30 20:33:05 +0530

    Update start-frontend.sh

--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -2,5 +2,8 @@
 sudo apt update
 sudo apt install -y python3
 
+# Go to frontend directory before starting the HTTP server
+cd /opt/gpt2-chat-deployment/frontend
+
 # Serve static frontend via Python HTTP server
-nohup python3 -m http.server 8080 &
+nohup python3 -m http.server 8080 > frontend.log 2>&1 &
