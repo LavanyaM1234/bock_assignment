bfc82f1082eda3b94e21fc9f4c5a1893e382c261
LavanyaM1234 <lavanyam7366@gmail.com>
2025-07-30 20:57:20 +0530

    Update start-frontend.sh

--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -4,6 +4,6 @@ sudo apt install -y python3
 
 # Go to frontend directory before starting the HTTP server
 cd /opt/gpt2-chat-deployment/frontend
-
+sed -i "s|{{BACKEND_IP}}|${backend_ip}|g" index.html
 # Serve static frontend via Python HTTP server
 nohup python3 -m http.server 8080 > frontend.log 2>&1 &
